9d17d350470a68010751f6ea3724c3f40c6efec6
kyie <yiek888@gmail.com>
2016-05-11 13:46:24 -0400

    Added the rest of the stuff

---
+++ OpenSniffer-0.1/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='OpenSniffer',
+      version='0.1',
+      description='Python module for OpenSniffer',
+      author='Sewio',
+      url='http://sewio.net',
+      py_modules=['opensniffer'],
+     )
